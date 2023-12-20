8bba43b3583bddc84212aee423df7c782b502c19
jbbisch <108563925+jbbisch@users.noreply.github.com>
2023-12-20 02:50:09 -0600

    Update startOrderStrategy.js

--- standardMiddleware/startOrderStrategy.js
+++ standardMiddleware/startOrderStrategy.js
@@ -1,45 +1,58 @@
 const { getSocket, getReplaySocket } = require("../websocket/utils")
+const { onChart } = require("../strategies/crossover/onChart")
 
 const startOrderStrategy = (state, action) => {
 
     const [event, payload] = action
+    //console.log('entry action', action)
 
-    if(event === 'crossover/draw') {
+    if(event === 'orderStrategy/startOrderStrategy') {
         const { data, props } = payload
-        const { dev_mode, dispatcher } = props
-        const { effects} = dispatcher
-        const { contract, action, brackets, entryVersion } = effects
+        const { dev_mode } = props
+        const { accountId, accountSpec, symbol, action, brackets, entryVersion } = data
+        console.log('Payload', payload)
+        //console.log('symbol', symbol)
+        //console.log('action', action)
+        //console.log('brackets', brackets)
+        //console.log('entryVersion', entryVersion)
 
         const socket = dev_mode ? getReplaySocket() : getSocket()
 
         const orderData = {
-            entryVersion,
-            brackets,
+            entryVersion: entryVersion,
+            brackets: brackets,
         }
-
+        console.log('orderData', orderData)
         console.log(JSON.stringify(orderData, null, 2))
         
         const body = {
-            accountId: parseInt(process.env.ID, 10),
-            accountSpec: process.env.SPEC,
-            symbol: contract.name,
+            accountId: accountId,
+            accountSpec: accountSpec,
+            symbol: symbol,
             action: action,
             orderStrategyTypeId: 2,
-            params: JSON.stringify(orderData)
+            isAutomated: true,
+            params: JSON.stringify(orderData),
         }
+
+        //authorize socket using your access token
+        socket.onopen = function() {
+            socket.request(`authorize\n0\n\n${process.env.ACCESS_TOKEN}`)
+        }
+
+        socket.request(`orderstrategy/startorderstrategy\n4\n\n${JSON.stringify(body)}`)
 
         let dispose = socket.request({
             url: 'orderStrategy/startOrderStrategy',
-            body,
+            body: body,
             callback: (id, r) => {
                 if(id === r.i) {
-                    console.log(JSON.stringify(r, null, 2))
+                    console.log(JSON.stringify('Order Placed for Buy', r, null, 2))
                     dispose()
                 }
             }
         })
     }
-
     return action
 }
 
