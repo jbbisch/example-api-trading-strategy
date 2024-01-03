33bbb069ad3429a7a5b28b71a8bd46d3f7ddc91f
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-01-02 19:51:27 -0600

    Update placeOrder.js

--- standardMiddleware/placeOrder.js
+++ standardMiddleware/placeOrder.js
@@ -1,35 +1,58 @@
 const { getSocket, getReplaySocket } = require("../websocket/utils")
 
-const placeOrder = (state, action) => {
+const placeOrder = async (state, action) => {
     const [event, payload] = action
 
     if(event === '/order/placeOrder') {
         const { data, props } = payload
+        console.log('[placeOrder] props', props)
         const { dev_mode } = props
-        const { contract, orderType, action, orderQty, price } = data
-
-        const socket = dev_mode ? getReplaySocket() : getSocket()
+        const { contract, action, entryVersion, price } = data
+        const { orderType, orderQty } = entryVersion
 
         const body = {
-            symbol: contract.name,
-            orderQty,
             accountSpec: process.env.SPEC,
             accountId: parseInt(process.env.ID, 10),
-            action,
-            price,
-            orderType,
-            isAutomated: true
-
+            action: action,
+            symbol: contract.name,
+            orderQty: orderQty,
+            orderType: orderType,
+            isAutomated: true,
+            price
         }
+        
+        const URL = 'demo.tradovateapi.com/v1'
 
-        let dispose = socket.request({
-            url: 'order/placeOrder',
-            body,
-            callback: (id, r) => {
-                console.log('Placed order successfully')
-                dispose()
-            }
+        const response = await fetch(URL + '/order/placeorder', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${myAccessToken}`,
+            },
+            body: JSON.stringify(body)
         })
+        
+        const json = await response.json()
+        
+//        const socket = dev_mode ? getReplaySocket() : getSocket()
+        
+//        socket.onOpen = function() {
+//            socket.request(`/authorize\n0\n\n${process.env.ACCESS_TOKEN}`)
+//        }
+
+//        let dispose = socket.request({
+//            url: process.env.WS_URL + `order/placeOrder\n4\n\n${JSON.stringify(body)}`,
+//            callback: (id, r) => {
+//                if (id === r.i) {
+//                    console.log('[placeOrder] Response from trying to place the order RSSS:', r.s)
+//                    console.log('[placeOrder] Response from trying to place the order RIII:', r.i)
+//                    console.log('[placeOrder] Response from trying to place the order RDDD:', r.d)
+//                    console.log('[placeOrder] Response from trying to place the order RRRR:', r)
+//                    console.log('Placed order successfully')
+//                    dispose()
+//                }
+//            }
+//        })
     }
     
 
