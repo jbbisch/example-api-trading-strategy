080e3cc1a7fa8b63e486f24c789d1313317b7da9
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-02-14 18:54:31 -0600

    liquidate update

--- standardMiddleware/placeOrder.js
+++ standardMiddleware/placeOrder.js
@@ -1,70 +1,83 @@
 const { getSocket, getReplaySocket } = require("../websocket/utils")
-const axios = require('axios')
+const { logger } = require("../utils/globalErrorHandler")
+
 
 const placeOrder = (state, action) => {
-    //console.log('PlaceOrder FUNCTION called')
-    if (Array.isArray(action)) {
+        //console.log('placeOrder FUNCTION called')
+    try {
         const [event, payload] = action
 
-        if(event === '/order/placeOrder') {
-            console.log('handling placeOrder EVENT')
+        if(event === 'order/liquidatePosition') {
+            console.log('HANDLING liquidatePosition EVENT:')
+
             const { data, props } = payload
-            console.log('[placeOrder] props', props)
             const { dev_mode } = props
-            const { contract, action, entryVersion, price } = data
-            const { orderType, orderQty } = entryVersion
+            const { contract, orderType, action, orderQty, price } = data
+
+            const socket = dev_mode ? getReplaySocket() : getSocket()
+
+            socket.onOpen = function() {
+                socket.request(`/authorize\n0\n\n${process.env.ACCESS_TOKEN}`)
+            }
 
             const body = {
+                symbol: contract,
+                orderQty: orderQty,
                 accountSpec: process.env.SPEC,
                 accountId: parseInt(process.env.ID, 10),
                 action: action,
-                symbol: contract.name,
-                orderQty: orderQty,
+                //price,
                 orderType: orderType,
-                isAutomated: true,
-                price
+                isAutomated: true
+
             }
-        
-//            const URL = 'https://demo.tradovateapi.com/v1'
 
-//            axios.post(URL + 'order/placeOrder', body, {
-//                headers: {
-//                    'Content-Type': 'application/json',
-//                    'Accept': 'application/json',
-//                    'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
-//                }
-//            })
-//            .then((response) => {
-//                console.log('[placeOrder] response', response)
-            
-//            })
-//            .catch((error) => {
-//                console.log('[placeOrder] error', error)
-//            })
-        
-        const socket = dev_mode ? getReplaySocket() : getSocket()
-        
-        socket.onOpen = function() {
-            socket.request(`/authorize\n0\n\n${process.env.ACCESS_TOKEN}`)
-        }
-
-        let dispose = socket.request({
-            url: process.env.WS_URL + `order/placeOrder\n4\n\n${JSON.stringify(body)}`,
-            callback: (id, r) => {
-                if (id === r.i) {
+            let dispose = socket.request({
+                url: process.env.WS_URL + `/order/liquidatePosition\n4\n\n${JSON.stringify(body)}`,
+                callback: (id, r) => {
                     console.log('[placeOrder] Response from trying to place the order RSSS:', r.s)
                     console.log('[placeOrder] Response from trying to place the order RIII:', r.i)
                     console.log('[placeOrder] Response from trying to place the order RDDD:', r.d)
                     console.log('[placeOrder] Response from trying to place the order RRRR:', r)
-                    console.log('Placed order successfully')
-                    dispose()
+                                    
+                    if (id === r.i) {
+                        switch (r.s) {
+                            case 200:
+                                console.log(JSON.stringify('Order placed 22222222222222200000000000000000 for buy', r.s, null, 2))
+                                break
+                            case 400:
+                                console.error(JSON.stringify('Failed to place44444444444444440000000000000000000000000000 buy order', r.d, null, 2))
+                                break
+                            case 401:
+                                console.error(JSON.stringify('Unauthorized 4444444444444444000000000000001111111111111', r.d, null, 2))
+                                break
+                            case 403:
+                                console.error(JSON.stringify('Forbidden 444444444440000000000000033333333333333', r.d, null, 2))
+                                break
+                            case 404:
+                                console.error(JSON.stringify('Not found 44444444444400000000000000444444444444444444444444444000000000000004444444444444444444444444440000000000000044444444444444444444444444400000000000000444444444444444444444444444000000000000004444444444444444444444444440000000000000044444444444444444444444444400000000000000444444444444444444444444444000000000000004444444444444444444444444440000000000000044444444444444444444444444400000000000000444444444444444444444444444000000000000004444444444444444444444444440000000000000044444444444444444444444444400000000000000444444444444444', r.d, null, 2))
+                                break
+                            case 500:
+                                console.error(JSON.stringify('Internal server error 5555555555555500000000000000000000000000', r.d, null, 2))
+                                break
+                            default:
+                                console.error(JSON.stringify('Unknown error', r.d, null, 2))
+                                break
+                        }
+                        if (r.s === 200) {
+                            console.log(JSON.stringify('Order placed for buy PLACED PLACED PLACED SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS', r.s, null, 2))
+                        } else {
+                            console.error(JSON.stringify('Failed to place the order FAILED FAILED FAILED FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', r.d, null, 2))
+                        }
+                        dispose()
+                    }    
                 }
-            }
-        })
+            })
+        }
+    } catch (error) {
+        console.error('Error in placeOrder', error.message, error.stack, error)
+        logger.error('Error in placeOrder', error.message, error.stack, error)
     }
-    }
-    
-
     return action
 }
 
