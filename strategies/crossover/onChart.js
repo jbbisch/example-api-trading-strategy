a963a516200c284d5a36a0e755a8ac4682e8b314
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-04-04 05:03:32 -0500

    Update onChart.js

--- strategies/crossover/onChart.js
+++ strategies/crossover/onChart.js
@@ -14,7 +14,7 @@ const onChart = (prevState, {data, props}) => {
     
     const lastTlc = tlc.state
 
-    const { negativeCrossover, positiveCrossover } = tlc(lastTlc, bufferData)
+    const { negativeCrossover, positiveCrossover, distance } = tlc(lastTlc, bufferData)
 
     const longBracket = {
         qty: orderQuantity,
@@ -38,190 +38,209 @@ const onChart = (prevState, {data, props}) => {
     const currentPositionSize = prevState.position?.netPos || 0
     
     const now = Date.now()
-    const chillOut = 3 * 60 * 1000 // 3 minutes pause after placing an order
+    const chillOut = 5 * 60 * 1000 // 5 minutes pause after placing an order
     if (prevState.lastTradeTime && now - prevState.lastTradeTime < chillOut) {
         console.log('[OnChart] Chill out time')
         return { state: prevState, effects: [] }
     }
 
     // USE DURING BEAR MARKET INSTEAD OF WATCH AND LONG ##########
-    // if(mode === LongShortMode.Watch && negativeCrossover) {
-    //     console.log('[onChart] liquidatePosition 1:', placeOrder)
-    //     console.log('[onChart] mode 1 placeOrder:', mode)
-    //     placeOrder({
-    //         accountId: parseInt(process.env.ID),
-    //         contractId: contract.id,
-    //         admin: true,
-    //         accountSpec: process.env.SPEC,
-    //         deviceId: process.env.DEVICE_ID,
-    //         symbol: contract.name,
-    //         action: "Sell",
-    //         orderQty: 1,
-    //         orderType: "Market"
-    //     }).then(response => {
-    //         prevState.lastTradeTime = Date.now()
-    //         console.log('[onChart] response 1:', response)
-    //         return {
-    //             state: {
-    //                 ...prevState,
-    //                 mode: LongShortMode.Short,
-    //             },
-    //             effects: [
-    //                  FOR WEBSOCKET Liquidates any existing position
-    //                 {
-    //                     url: 'order/liquidatePosition', 
-    //                     data: {
-    //                         accountId: parseInt(process.env.ID),
-    //                         contractId: contract.id,
-    //                         admin: true,
-    //                         accountSpec: process.env.SPEC,
-    //                         deviceId: process.env.DEVICE_ID,
-    //                         symbol: contract.name,
-    //                         action: "Sell",
-    //                         orderQuantity: orderQuantity,
-    //                     }   
-    //                 },
-    //                 { event: 'crossover/draw' },
-    //             ],
-    //         }
-    //     }).catch(err => {
-    //         console.error('[onChart] Error:', err)
-    //     })
+    // if(mode === LongShortMode.Watch && negativeCrossover ) {
+    //    if(currentPositionSize === 0) {
+        //     console.log('[onChart] liquidatePosition 1:', placeOrder)
+        //     console.log('[onChart] mode 1 placeOrder:', mode)
+        //     placeOrder({
+        //         accountId: parseInt(process.env.ID),
+        //         contractId: contract.id,
+        //         admin: true,
+        //         accountSpec: process.env.SPEC,
+        //         deviceId: process.env.DEVICE_ID,
+        //         symbol: contract.name,
+        //         action: "Sell",
+        //         orderQty: 1,
+        //         orderType: "Market"
+        //     }).then(response => {
+        //         prevState.lastTradeTime = Date.now()
+        //         console.log('[onChart] response 1:', response)
+        //         return {
+        //             state: {
+        //                 ...prevState,
+        //                 mode: LongShortMode.Short,
+        //             },
+        //             effects: [
+        //                  FOR WEBSOCKET Liquidates any existing position
+        //                 {
+        //                     url: 'order/liquidatePosition', 
+        //                     data: {
+        //                         accountId: parseInt(process.env.ID),
+        //                         contractId: contract.id,
+        //                         admin: true,
+        //                         accountSpec: process.env.SPEC,
+        //                         deviceId: process.env.DEVICE_ID,
+        //                         symbol: contract.name,
+        //                         action: "Sell",
+        //                         orderQuantity: orderQuantity,
+        //                     }   
+        //                 },
+        //                 { event: 'crossover/draw' },
+        //             ],
+        //         }
+        //     }).catch(err => {
+        //         console.error('[onChart] Error:', err)
+        //     })
+        // } else {
+        //    console.log('[onChart] short position already exists')
+        //    return { state: prevState, effects: [] }
+        // }
     // }
 
     // USE DURING BEAR MARKET INSTEAD OF WATCH AND LONG ##########    
-    // if(mode === LongShortMode.Short && positiveCrossover) { 
-    //     console.log('[onChart] placeOrder 4:', placeOrder)
-    //     console.log('[onChart] mode 4 buyOrder:', mode)
-    //     placeOrder({
-    //         accountId: parseInt(process.env.ID),
-    //         contractId: contract.id,
-    //         admin: true,
-    //         accountSpec: process.env.SPEC,
-    //         deviceId: process.env.DEVICE_ID,
-    //         symbol: contract.name,
-    //         action: "Buy",
-    //         orderQty: 1,
-    //         orderType: "Market"
-    //     }).then(response => {
-    //         prevState.lastTradeTime = Date.now()
-    //         console.log('[onChart] response 4:', response)
-    //         return {
-    //             state: {
-    //                 ...prevState,
-    //                 mode: LongShortMode.Long,
-    //             },
-    //             effects: [
-    //                     FOR WEBSOCKET
-    //                 {
-    //                     url: 'orderStrategy/startOrderStrategy',
-    //                     data: {
-    //                         accountId: parseInt(process.env.ID),
-    //                         accountSpec: process.env.SPEC,
-    //                         symbol: contract.id,
-    //                         action: "Buy",
-    //                         orderStrategyTypeId: 2,
-    //                         entryVersion: JSON.stringify(entryVersion),
-    //                         brackets: JSON.stringify(longBracket),
-    //                     }
-    //                 },
-    //                 { event: 'crossover/draw' },
-    //             ],
-    //         }
-    //     }).catch(err => {
-    //         console.error('[onChart] Error:', err)
-    //     })
+    // if(mode === LongShortMode.Short && positiveCrossover ) { 
+        //    if(currentPositionSize < 0) {
+        //     console.log('[onChart] placeOrder 4:', placeOrder)
+        //     console.log('[onChart] mode 4 buyOrder:', mode)
+        //     placeOrder({
+        //         accountId: parseInt(process.env.ID),
+        //         contractId: contract.id,
+        //         admin: true,
+        //         accountSpec: process.env.SPEC,
+        //         deviceId: process.env.DEVICE_ID,
+        //         symbol: contract.name,
+        //         action: "Buy",
+        //         orderQty: 1,
+        //         orderType: "Market"
+        //     }).then(response => {
+        //         prevState.lastTradeTime = Date.now()
+        //         console.log('[onChart] response 4:', response)
+        //         return {
+        //             state: {
+        //                 ...prevState,
+        //                 mode: LongShortMode.Long,
+        //             },
+        //             effects: [
+        //                     FOR WEBSOCKET
+        //                 {
+        //                     url: 'orderStrategy/startOrderStrategy',
+        //                     data: {
+        //                         accountId: parseInt(process.env.ID),
+        //                         accountSpec: process.env.SPEC,
+        //                         symbol: contract.id,
+        //                         action: "Buy",
+        //                         orderStrategyTypeId: 2,
+        //                         entryVersion: JSON.stringify(entryVersion),
+        //                         brackets: JSON.stringify(longBracket),
+        //                     }
+        //                 },
+        //                 { event: 'crossover/draw' },
+        //             ],
+        //         }
+        //     }).catch(err => {
+        //         console.error('[onChart] Error:', err)
+        //     })
+        // } else {
+        //    console.log('[onChart] no short position to liquidate')
+        //    return { state: prevState, effects: [] }
+        // }    
     // }
 
     // USE DURING BULL MARKET INSTEAD OF WATCH AND SHORT ##########
-    if(mode === LongShortMode.Long && negativeCrossover && currentPositionSize >= maxPosition) { 
-        console.log('[onChart] liquidatePosition 2:', placeOrder)
-        console.log('[onChart] mode 2 placeOrder:', mode)
-        placeOrder({
-            accountId: parseInt(process.env.ID),
-            contractId: contract.id,
-            admin: true,
-            accountSpec: process.env.SPEC,
-            deviceId: process.env.DEVICE_ID,
-            symbol: contract.name,
-            action: "Sell",
-            orderQty: 1,
-            orderType: "Market"
-        }).then(response => {
-            prevState.lastTradeTime = Date.now()
-            console.log('[onChart] response 2:', response)
-            return {
-                state: {
-                    ...prevState,
-                    mode: LongShortMode.Short,
-                },
-                effects: [
-                    // FOR WEBSOCKET Liquidates any existing position
-                    // {
-                    //     url: 'order/liquidatePosition',
-                    //     data: {
-                    //         accountId: parseInt(process.env.ID),
-                    //         contractId: contract.id,
-                    //         admin: true,
-                    //         accountSpec: process.env.SPEC,
-                    //         deviceId: process.env.DEVICE_ID,
-                    //         symbol: contract.name,
-                    //         action: "Sell",
-                    //         orderQuantity: orderQuantity,
-                    //     }
-                    // },
-                    { event: 'crossover/draw' },
-                ],
-            }
-        }).catch(err => {
-            console.error('[onChart] Error:', err)
-        })
+    if(mode === LongShortMode.Long && negativeCrossover ) {
+        if(currentPositionSize >= maxPosition) {
+            console.log('[onChart] liquidatePosition 2:', placeOrder)
+            console.log('[onChart] mode 2 placeOrder:', mode)
+            placeOrder({
+                accountId: parseInt(process.env.ID),
+                contractId: contract.id,
+                admin: true,
+                accountSpec: process.env.SPEC,
+                deviceId: process.env.DEVICE_ID,
+                symbol: contract.name,
+                action: "Sell",
+                orderQty: 1,
+                orderType: "Market"
+            }).then(response => {
+                prevState.lastTradeTime = Date.now()
+                console.log('[onChart] response 2:', response)
+                return {
+                    state: {
+                        ...prevState,
+                        mode: LongShortMode.Short,
+                    },
+                    effects: [
+                        // FOR WEBSOCKET Liquidates any existing position
+                        // {
+                        //     url: 'order/liquidatePosition',
+                        //     data: {
+                        //         accountId: parseInt(process.env.ID),
+                        //         contractId: contract.id,
+                        //         admin: true,
+                        //         accountSpec: process.env.SPEC,
+                        //         deviceId: process.env.DEVICE_ID,
+                        //         symbol: contract.name,
+                        //         action: "Sell",
+                        //         orderQuantity: orderQuantity,
+                        //     }
+                        // },
+                        { event: 'crossover/draw' },
+                    ],
+                }
+            }).catch(err => {
+                console.error('[onChart] Error:', err)
+            })
+        } else {
+            console.log('[onChart] no position to liquidate')
+            return { state: prevState, effects: [] }
+        }
     }
 
     // USE DURING BULL MARKET INSTEAD OF WATCH AND SHORT ##########
-    if(mode === LongShortMode.Watch && positiveCrossover && currentPositionSize < maxPosition) { 
-        console.log('[onChart] placeOrder 3:', placeOrder)
-        console.log('[onChart] mode 3 buyOrder:', mode)  
-        placeOrder({
-            accountId: parseInt(process.env.ID),
-            contractId: contract.id,
-            admin: true,
-            accountSpec: process.env.SPEC,
-            deviceId: process.env.DEVICE_ID,
-            symbol: contract.name,
-            action: "Buy",
-            orderQty: 1,
-            orderType: "Market"
-        }).then(response => {
-            prevState.lastTradeTime = Date.now()
-            console.log('[onChart] response 3:', response)
-            return {
-                state: {
-                    ...prevState,
-                    mode: LongShortMode.Long,
-                },
-                effects: [
-                    // FOR WEBSOCKET
-                    // {
-                    //     url: 'orderStrategy/startOrderStrategy',
-                    //     data: {
-                    //         accountId: parseInt(process.env.ID),
-                    //         accountSpec: process.env.SPEC,
-                    //         symbol: contract.id,
-                    //         action: "Buy",
-                    //         orderStrategyTypeId: 2,
-                    //         entryVersion: JSON.stringify(entryVersion),
-                    //         brackets: JSON.stringify(longBracket),
-                    //     }   
-                    // },
-                    { event: 'crossover/draw' },
-                ],
-            }
-        }).catch(err => {
-            console.error('[onChart] Error:', err)
-        })
+    if(mode === LongShortMode.Watch && positiveCrossover ) {
+        if(currentPositionSize < maxPosition) { 
+            console.log('[onChart] placeOrder 3:', placeOrder)
+            console.log('[onChart] mode 3 buyOrder:', mode)  
+            placeOrder({
+                accountId: parseInt(process.env.ID),
+                contractId: contract.id,
+                admin: true,
+                accountSpec: process.env.SPEC,
+                deviceId: process.env.DEVICE_ID,
+                symbol: contract.name,
+                action: "Buy",
+                orderQty: 1,
+                orderType: "Market"
+            }).then(response => {
+                prevState.lastTradeTime = Date.now()
+                console.log('[onChart] response 3:', response)
+                return {
+                    state: {
+                        ...prevState,
+                        mode: LongShortMode.Long,
+                    },
+                    effects: [
+                        // FOR WEBSOCKET
+                        // {
+                        //     url: 'orderStrategy/startOrderStrategy',
+                        //     data: {
+                        //         accountId: parseInt(process.env.ID),
+                        //         accountSpec: process.env.SPEC,
+                        //         symbol: contract.id,
+                        //         action: "Buy",
+                        //         orderStrategyTypeId: 2,
+                        //         entryVersion: JSON.stringify(entryVersion),
+                        //         brackets: JSON.stringify(longBracket),
+                        //     }   
+                        // },
+                        { event: 'crossover/draw' },
+                    ],
+                }
+            }).catch(err => {
+                console.error('[onChart] Error:', err)
+            })
+        } else {
+            console.log('[onChart] max position reached')
+            return { state: prevState, effects: [] }
+        }
     }
-
     return { state: prevState, effects: [] }
 }
 
