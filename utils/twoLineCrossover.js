e35ee6069a4395c5da43e0a42e2903089741ada4
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-05-11 03:04:19 -0500

    Update twoLineCrossover.js

--- utils/twoLineCrossover.js
+++ utils/twoLineCrossover.js
@@ -9,12 +9,15 @@ module.exports = function twoLineCrossover(shortPeriod, longPeriod) {
         const longSma = newData.slice(newData.length - longPeriod).reduce((a, b) => a + b.close || b.price, 0)/longPeriod
         const distance = shortSma - longSma
 
+        const positiveCrossover = prevState.distance < 0.00 && distance > 0.00
+        const negativeCrossover = prevState.distance > 0.17 && distance < 0.17
+
         next = {
             shortSma: shortSma,
             longSma: longSma,
             distance: distance,
-            positiveCrossover: prevState.shortSma <= prevState.longSma && distance > 0.00,
-            negativeCrossover: prevState.shortSma >= prevState.longSma && distance < 0.00,
+            positiveCrossover: positiveCrossover, //prevState.shortSma <= prevState.longSma && distance > 0.00,
+            negativeCrossover: negativeCrossover, //prevState.shortSma >= prevState.longSma && distance < 0.00,
         }         
 
         console.log('Updating state with new SMA values: Previous State - Short SMA: ', prevState.shortSma, ' Long SMA: ', prevState.longSma, ' Current State - Short SMA: ', next.shortSma, ' Long SMA: ', next.longSma, ' Distance: ', next.distance, ' Positive Crossover: ', next.positiveCrossover, ' Negative Crossover: ', next.negativeCrossover)
