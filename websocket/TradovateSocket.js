8a5ab256555a1395a6081b857845cab4d8b33668
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-06-07 22:54:28 -0500

    Update TradovateSocket.js

--- websocket/TradovateSocket.js
+++ websocket/TradovateSocket.js
@@ -273,6 +273,10 @@ TradovateSocket.prototype.reconnect = function() {
             }
         }
         checkClosedAndReconnect()
+        
+        if (this.mdSocket) {
+            this.mdSocket.mdReconnect();  // Call mdReconnect to handle market data socket reconnection
+        }
         }, Math.pow(2, this.reconnectAttempts) * 1000)
         this.reconnectAttempts += 1
     }
