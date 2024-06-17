4a4e5e46207e3a22a767710f42d3c562154d93b0
jbbisch <108563925+jbbisch@users.noreply.github.com>
2024-06-17 18:52:58 -0500

    Update crossoverDrawEffect.js

--- strategies/crossover/crossoverDrawEffect.js
+++ strategies/crossover/crossoverDrawEffect.js
@@ -8,18 +8,18 @@ const drawEffect = (state, action) => {
         const { props } = payload
         const { contract } = props
         const { product, position, mode, buffer, tlc, realizedPnL } = state
-        const { distance, shortSma, longSma, momentum, shortSmaValues,distanceMomentum, distanceValues } = tlc.state  
+        const { distance, shortSma, longSma, momentum, shortSmaValues,distanceMomentum, distanceValues, momentumPeak, distancePeak } = tlc.state  
 
         drawToConsole({
             mode,
             contract: contract.name,      
             netPos: position?.netPos || 0,
-            Distance: distance.toFixed(2),
             DistanceMomentum: distanceMomentum.toFixed(8),
             'Distance Values': distanceValues.map(value => value.toFixed(2)).join(', '),
-            Momentum: momentum.toFixed(8),
+            DistancePeak: distancePeak,
+            MomentumPeak: momentumPeak,
+            ShortSmaMomentum: momentum.toFixed(8),
             'Short SMA Values': shortSmaValues.map(value => value.toFixed(2)).join(', '),
-            SHORTsma: shortSma.toFixed(2),
             LONGsma: longSma.toFixed(2),
             'p&l': position && position.netPos !== 0 && product 
                 ? `$${calculatePnL({
