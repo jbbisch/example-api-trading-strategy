ab4f28e750b0c61b34dca08cea39f27d790e4411
jbbisch <108563925+jbbisch@users.noreply.github.com>
2025-06-24 09:01:10 -0400

    Update drawToConsole.js

--- utils/drawToConsole.js
+++ utils/drawToConsole.js
@@ -2,6 +2,6 @@ module.exports = function drawToConsole(items, clear = true) {
     if(clear) console.clear()
     console.log(`\n[AutoTrade]`)
     Object.entries(items).forEach(([k, v]) => {
-        console.log(`${k}:\n${JSON.stringify(v, null, 2)}`)
+        console.log(`${k}:\n${v}\n`)
     })
 }
